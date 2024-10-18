1aa2480864ff40bc21dbb99a8b22def4c2477749
Jannis Gehring <jannis-gehring@t-online.de>
2024-10-18 14:23:58 +0200

    remove getops stuff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-import getopt
 from urllib.request import urlopen
 import requests
 import io
@@ -68,47 +67,38 @@ def read_numbers_from_file(filename):
     with open(filename, 'r') as file:
         numbers = [int(line.strip()) for line in file if line.strip().isdigit()]
     return numbers
-
-try:
-    opts, args = getopt.gnu_getopt(sys.argv[1:],
-                                    "h",
-                                    ["help"])
-except getopt.GetoptError as err:
-    print(sys.argv[0],":", err)
-    sys.exit(1)
-
-processOptions(opts)
-riders: list[Rider] = []
-ids = read_numbers_from_file("riders.txt")
-exceptions = []
-for id in ids:
-    rider = getRider(id)
-    if rider is not None:
-        riders.append(rider)
-    print(rider)
-riders = sorted(riders, key=lambda rider: (-rider.distance, rider.getHours()))
-output = "Nummer,Name,Distanz,Zeit,Geschwindigkeit\n"
-riderCtr = {
-    "90": 0,
-    "170": 0,
-    "220": 0,
-    "270": 0,
-    "310": 0
-}
-currentDistance = 0
-formerDistance = 0
-i = 0
-for rider in riders:
-    if formerDistance == rider.distance:
-        i+=1
-    else:
-        i=1
-    output+=f"{i},{rider.name};{rider.distance};{rider.time};{rider.getVelocity()}\n"
-    riderCtr[str(rider.distance)]+=1
-    formerDistance = rider.distance
-output = output.replace(".",",")
-with open("albextremOutput.csv", "w") as outputFile:
-        outputFile.write(output)
-        outputFile.close()
-print(f"In List: {riderCtr['90']+riderCtr['170']+riderCtr['220']+riderCtr["270"]+riderCtr['310']}")
-print(f"Not in list: {len(exceptions)}")
+if __name__ == "__main__":
+    riders: list[Rider] = []
+    ids = read_numbers_from_file("riders.txt")
+    exceptions = []
+    for id in ids:
+        rider = getRider(id)
+        if rider is not None:
+            riders.append(rider)
+        print(rider)
+    riders = sorted(riders, key=lambda rider: (-rider.distance, rider.getHours()))
+    output = "Nummer,Name,Distanz,Zeit,Geschwindigkeit\n"
+    riderCtr = {
+        "90": 0,
+        "170": 0,
+        "220": 0,
+        "270": 0,
+        "310": 0
+    }
+    currentDistance = 0
+    formerDistance = 0
+    i = 0
+    for rider in riders:
+        if formerDistance == rider.distance:
+            i+=1
+        else:
+            i=1
+        output+=f"{i},{rider.name};{rider.distance};{rider.time};{rider.getVelocity()}\n"
+        riderCtr[str(rider.distance)]+=1
+        formerDistance = rider.distance
+    output = output.replace(".",",")
+    with open("albextremOutput.csv", "w") as outputFile:
+            outputFile.write(output)
+            outputFile.close()
+    print(f"In List: {riderCtr['90']+riderCtr['170']+riderCtr['220']+riderCtr["270"]+riderCtr['310']}")
+    print(f"Not in list: {len(exceptions)}")
